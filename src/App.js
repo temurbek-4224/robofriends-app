82b7327da7a739d5e1c7996b8f1629f272e9fd15
temurbek-4224 <78374407+temurbek-4224@users.noreply.github.com>
2021-04-19 11:28:24 +0500

    Now I have created robofriends app width Neagoie

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React,{ Component } from 'react';
+import CardList from './CardList';
+import {robots} from './robots';
+import SearchBox from './SearchBox';
+import './App.css';
+
+class App extends Component{
+	constructor(){
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) =>{
+		this.setState({ searchfield: event.target.value})
+	}
+
+	render(){
+		const filteredRobots = this.state.robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		});
+		return(
+			<div className = 'tc'>
+			  <h1 className='f1'>Robo Friends</h1>
+			  <SearchBox searchChange = {this.onSearchChange}/>
+				<CardList robots = {filteredRobots}/>
+			</div>
+		);
+	}
+}
+
+export default App;
